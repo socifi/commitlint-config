b07a4cc2ddbb7da712e0e1af0f6dba3b6ed4ef3e
Michal Ozogán <michal@ozogan.eu>
2018-10-04 12:30:24 +0200

    Test: RND-15 Testing

--- tests/config.test.js
+++ tests/config.test.js
@@ -7,9 +7,9 @@ function validateCommit(commit) {
 
 describe('Test commit message linter settings', async () => {
     it('Requires type', async () => {
-        expect((await validateCommit(': Abcd. CON-9')).errors.length).toBeGreaterThan(0);
-        expect((await validateCommit('changed: Abcd. CON-9')).errors.length).toBeGreaterThan(0);
-        expect((await validateCommit('unknown: Abcd. CON-9')).errors.length).toBeGreaterThan(0);
+        expect((await validateCommit(': CON-9 Abcd.')).errors.length).toBeGreaterThan(0);
+        expect((await validateCommit('changed: CON-9 Abcd.')).errors.length).toBeGreaterThan(0);
+        expect((await validateCommit('unknown: CON-9 Abcd.')).errors.length).toBeGreaterThan(0);
     });
 
     it('Requires message', async () => {
@@ -21,7 +21,11 @@ describe('Test commit message linter settings', async () => {
         expect((await validateCommit('Fixed: Abcd')).errors.length).toBeGreaterThan(0);
     });
 
-    it('Validate type', async () => {
-        expect((await validateCommit('InProgress: Abcd. CON-9')).errors).toEqual([]);
+    it('Validate reference', async () => {
+        expect((await validateCommit('InProgress: CON-9 Abcd.')).errors).toEqual([]);
+    });
+
+    it('Validate multiline commit', async () => {
+        expect((await validateCommit('Fixed: CON-9 Abcd.\nX: CON-10 Abcd.')).errors).toEqual([]);
     });
 });
